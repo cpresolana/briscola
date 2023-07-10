fe1cffbefb271a819185633458391af326f1372d
Christian Presolana <cpreso01@it-pv7tdc2qy4.emea.media.global.loc>
2023-07-10 14:06:29 +0200

    fix config.js WEB_SEVER_URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const GAME_NAME = 'Briscola';
 export const GAME_SERVER_PORT = 8000;
 export const GAME_SERVER_URL = `http://localhost:${GAME_SERVER_PORT}`;
-export const WEB_SERVER_URL = 'https://64abd8b07ed34f43f42eb2e7--gentle-paprenjak-a9793b.netlify.app';
+export const WEB_SERVER_URL = 'https://majestic-liger-098ba7.netlify.app/';
 export const APP_PRODUCTION = true;
