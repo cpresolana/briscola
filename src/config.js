0b3ac95209d7cf8a312197c7e2c9516a19adad40
cpresolanaisobar <christian.presolana@dentsu.com>
2023-07-12 10:02:58 +0200

    .

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const GAME_NAME = 'Briscola';
 export const GAME_SERVER_PORT = 8000;
 export const GAME_SERVER_URL = `https://briscola-server.onrender.com:${GAME_SERVER_PORT}`;
 export const WEB_SERVER_URL = 'https://briscola-cyan.vercel.app/';
-export const APP_PRODUCTION = true;
+export const APP_PRODUCTION = false;
