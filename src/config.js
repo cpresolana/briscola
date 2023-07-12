cbb948a59d19e357290042282cf64250ed37b960
cpresolanaisobar <christian.presolana@dentsu.com>
2023-07-12 09:34:15 +0200

    prepare config.js for prod

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const GAME_NAME = 'Briscola';
 export const GAME_SERVER_PORT = 8000;
-export const GAME_SERVER_URL = `http://localhost:${GAME_SERVER_PORT}`;
-export const WEB_SERVER_URL = 'https://majestic-liger-098ba7.netlify.app/';
+export const GAME_SERVER_URL = `https://briscola-server.onrender.com:${GAME_SERVER_PORT}`;
+export const WEB_SERVER_URL = 'https://briscola-server.onrender.com';
 export const APP_PRODUCTION = true;
